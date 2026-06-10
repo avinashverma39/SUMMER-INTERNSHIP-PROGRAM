0b7c86ec71214f180721280791ab0f3e1f788889
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-10 23:51:19 +0530

    Add some new code

---
+++ DAY_3/operator4.java
@@ -0,0 +1,11 @@
+package DAY_3;
+public class operator4
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int b=20;
+        int c=a+b;
+        System.out.println("The sum of a and b is: "+c);
+    }
+}

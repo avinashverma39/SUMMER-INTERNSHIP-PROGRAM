7a0f5011df1f6ac4691cff67a565bf77939eebfb
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-12 13:33:24 +0530

    Add

---
+++ DAY_5/loops.java
@@ -0,0 +1,13 @@
+package DAY_5;
+
+// loops in java
+public class loops {
+
+    public static void main(String[] args) {
+        // for loop
+        for (int i = 1; i < 11; i++) {
+            System.out.println(i);
+        }
+
+    }
+}

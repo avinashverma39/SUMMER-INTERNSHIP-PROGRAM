72d50d5f27f7430e00ab678c46f18f1e48622298
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-12 13:23:09 +0530

    Add

---
+++ DAY_5/assignmentOP.java
@@ -0,0 +1,13 @@
+package DAY_5;
+
+// assignment operator
+public class assignmentOP {
+
+    public static void main(String[] args) {
+        int a = 10;
+        System.out.println(a);
+        System.out.println(a+=2);
+        System.out.println(a*=3);
+        System.out.println(a/=4);
+    }
+}
---
+++ DAY_5/logical.java
@@ -0,0 +1,27 @@
+package DAY_5;
+
+// logical operator and, or, not
+public class logical {
+    public static void main(String[] args) {
+        // boolean a = true;
+        // boolean b = false;
+        // System.out.println(a && b);
+        // boolean c = true;
+        // boolean d = true;
+        // System.out.println(c && d);
+        // boolean e = false;
+        // boolean f = false;
+        // System.out.println(e && f);
+
+        boolean a = true;
+        boolean b = false;
+        System.out.println(a || b);
+        boolean c = true;
+        boolean d = true;
+        System.out.println(c || d);
+        boolean e = false;
+        boolean f = false;
+        System.out.println(e || f);
+        
+    }
+}

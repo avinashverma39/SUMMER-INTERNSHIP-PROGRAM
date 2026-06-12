1465c988384e2b1aba102ed57ba2fd45b367c88f
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-12 14:06:23 +0530

    Check prime

--- DAY_5/checkPrime.java
+++ DAY_5/checkPrime.java
@@ -5,7 +5,7 @@ public class checkPrime {
 
     public static void main(String args[]) {
 
-        int n = 29;
+        int n = 7;
         int isPrime = 0;
         for (int i = 2; i <= n / 2; i++) {
             if (n % i == 0) {
@@ -13,6 +13,12 @@ public class checkPrime {
                 break;
             }
         }
-        System.out.println(isPrime);
+            if (isPrime == 0) {
+                System.out.println(n + " is a prime number");
+            }
+            else {
+                System.out.println(n + " is not a prime number");
+            }
+        
     }
 }

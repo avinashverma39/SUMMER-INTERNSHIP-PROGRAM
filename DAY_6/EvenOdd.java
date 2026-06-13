01e098e477bcf47e64dd8a2dc93bebf3c7d8cf88
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-13 13:49:56 +0530

    Even odd using ternery operator

--- DAY_6/EvenOdd.java
+++ DAY_6/EvenOdd.java
@@ -1,12 +1,15 @@
 package DAY_6;
 
-// even odd number using ternery operator
+// even odd number using ternary operator
 public class EvenOdd {
 
     public static void main(String[] args) {
         int num = 10;
         String result = (num % 2 == 0) ? "Even" : "Odd";
         System.out.println(num + " is " + result);
+        
+        boolean result1 = (num % 2 == 0) ? true : false;
+        System.out.println(num + " is " + (result1 ? "Even" : "Odd"));
     }
 
 }

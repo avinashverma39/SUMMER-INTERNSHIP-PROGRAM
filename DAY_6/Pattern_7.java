423a402a3bad4022efb3107ab60f9773aab06c8e
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-14 02:18:18 +0530

    Create Pattern_7.java

---
+++ DAY_6/Pattern_7.java
@@ -0,0 +1,21 @@
+
+package DAY_6;
+
+public class Pattern_7 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if () {
+                    System.out.print("  ");
+                } else {
+                    System.out.print("*  ");
+                }
+            }
+
+            
+            System.out.println();
+        }
+    }
+
+
+}

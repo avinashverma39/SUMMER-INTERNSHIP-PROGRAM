f093f886a528eaf5d5ec3298199e90c85b622ddf
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 15:20:19 +0530

    Local  Global Variable and static variable

--- DAY_7/Local_global.java
+++ DAY_7/Local_global.java
@@ -4,12 +4,13 @@ package DAY_7;
 public class Local_global {
 
     int globalVar = 10; // global variable
-
+    static int staticVar = 30; // static variable
     public static void main(String[] args) {
         Local_global obj = new Local_global();
 
         int localVar = 20; // local variable
         System.out.println("Global variable: " + obj.globalVar);
+        System.out.println("Static variable: " + Local_global.staticVar);
         System.out.println("Local variable: " + localVar);
     }
 }

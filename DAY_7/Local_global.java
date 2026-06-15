a97c570042426913e8288ef65f2a4647bb9428bd
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 15:28:17 +0530

    Loops in function

--- DAY_7/Local_global.java
+++ DAY_7/Local_global.java
@@ -3,14 +3,24 @@ package DAY_7;
 //program to local and global variable
 public class Local_global {
 
-    int globalVar = 10; // global variable
-    static int staticVar = 30; // static variable
+    // int globalVar = 10; // global variable
+    // static int staticVar = 30; // static variable\
+
+    void looVar() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("This is a loop variable: " + i); // loop variable
+        }
+
+    }
+
     public static void main(String[] args) {
         Local_global obj = new Local_global();
-
-        int localVar = 20; // local variable
-        System.out.println("Global variable: " + obj.globalVar);
-        System.out.println("Static variable: " + Local_global.staticVar);
-        System.out.println("Local variable: " + localVar);
+        obj.looVar();
+        
+        // int localVar = 20; // local variable
+        // System.out.println("Global variable: " + obj.globalVar);
+        // System.out.println("Static variable: " + Local_global.staticVar);
+        // System.out.println("Local variable: " + localVar);
+        // }
     }
 }

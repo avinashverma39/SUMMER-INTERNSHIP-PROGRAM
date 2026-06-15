59733d029107d1ed02903e0df476cd98ac28e160
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 13:58:01 +0530

    My intro using function

--- DAY_7/My_intro.java
+++ DAY_7/My_intro.java
@@ -1,15 +1,25 @@
 package DAY_7;
 
 public class My_intro {
+      
+    int roll = 12345;
+    String name = "Avinash";
+    String College = "RR College";
+    Double percentage = 75.5;
+
 
     void display() {
-        System.out.println("My name is Avinash");
-        System.out.println("I am from Basti");
-        System.out.println("I am a student of Computer Science");
-        System.out.println("Curently I am in  year of my graduation");
-        System.out.println("Curently I am learning Java");
-        System.out.println("I am interested in programming and software development");
-
+        // System.out.println("My name is Avinash");
+        // System.out.println("I am from Basti");
+        // System.out.println("I am a student of Computer Science");
+        // System.out.println("Curently I am in  year of my graduation");
+        // System.out.println("Curently I am learning Java");
+        // System.out.println("I am interested in programming and software development");
+        
+        System.out.println("My Roll is: " + roll);
+        System.out.println("My Name is: " + name);
+        System.out.println("My College is: " + College);
+        System.out.println("My Percentage is: " + percentage);
     }
 
     public static void main(String[] args) {

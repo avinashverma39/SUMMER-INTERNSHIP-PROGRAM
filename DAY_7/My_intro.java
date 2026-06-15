2a4e8215c8414a9830442a3cff2ab54fda8e3538
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 13:41:23 +0530

    My intro using Function

---
+++ DAY_7/My_intro.java
@@ -0,0 +1,20 @@
+package DAY_7;
+
+public class My_intro {
+
+    void display() {
+        System.out.println("My name is Avinash");
+        System.out.println("I am from Basti");
+        System.out.println("I am a student of Computer Science");
+        System.out.println("Curently I am in  year of my graduation");
+        System.out.println("Curently I am learning Java");
+        System.out.println("I am interested in programming and software development");
+
+    }
+
+    public static void main(String[] args) {
+        My_intro obj = new My_intro(); // Object creation
+        obj.display(); // Function calling
+    }
+
+}

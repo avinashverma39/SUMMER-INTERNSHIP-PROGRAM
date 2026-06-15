484b498e6012593246d4f1e09e3adcf9922398f8
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 12:49:21 +0530

    Update Pattern_7.java

--- DAY_6/Pattern_7.java
+++ DAY_6/Pattern_7.java
@@ -5,7 +5,7 @@ public class Pattern_7 {
     public static void main(String[] args) {
         for (int i = 1; i <= 7; i++) {
             for (int j = 1; j <= 7; j++) {
-                if () {
+                if (i==3) {
                     System.out.print("  ");
                 } else {
                     System.out.print("*  ");

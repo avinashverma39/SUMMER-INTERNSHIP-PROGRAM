b331b8dd4e16b781466337ea304db9cc3e3b9201
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-15 13:18:24 +0530

    Function

---
+++ DAY_7/Funcion_1.java
@@ -0,0 +1,17 @@
+package DAY_7;
+
+public class Funcion_1 {
+
+    void display() {
+        System.out.println("Hello");
+    }
+
+    public static void main(String[] args) {
+        Funcion_1 obj = new Funcion_1(); // Object creation
+
+        System.out.println("Program Start");
+        obj.display(); // Function calling
+        System.out.println("Program End");
+    }
+
+}

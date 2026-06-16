9abd1e13d4921cbcca4526e19b2a151c65a26a43
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-16 13:17:07 +0530

    Use of int return

--- DAY_8/Compare_3_num.java
+++ DAY_8/Compare_3_num.java
@@ -2,9 +2,11 @@ package DAY_8;
 
 //larger between 3 number using function`
 public class Compare_3_num {
-    void compare(int a, int b, int c) {
+    int compare(int a, int b, int c) {
+
         int largest = (a > b) ? (a > c ? a : c) : (b > c ? b : c);
         System.out.println("The largest number is:" + largest);
+        return largest;
     }
 
     public static void main(String[] args) {
@@ -13,6 +15,7 @@ public class Compare_3_num {
         int n3 = 15;
 
         Compare_3_num obj = new Compare_3_num(); // Instance of class
-        obj.compare(n1, n2, n3);// function call
+        int result = obj.compare(n1, n2, n3); // function call
+        System.out.println("The result is: " + result);
     }
 }

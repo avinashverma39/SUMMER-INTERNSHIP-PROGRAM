8b558c815450a759baa879c0b6e038863794bc90
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-16 14:23:25 +0530

    Using without return type function overloding

--- DAY_8/OverLodingSum.java
+++ DAY_8/OverLodingSum.java
@@ -1,23 +1,23 @@
 package DAY_8;
 
 public class OverLodingSum {
-    int add(int a, int b) {
-        return a + b;
+    void add(int a, int b) {
+        System.out.println("Sum of a and b: " + (a + b));
     }
 
-    int add(int a, int b, int c) {
-        return a + b + c;
+    void add(int a, int b, int c) {
+        System.out.println("Sum of a, b and c: " + (a + b + c));
     }
 
-    int add(int a, int b, int c, int d) {
-        return a + b + c + d;
+    void add(int a, int b, int c, int d) {
+        System.out.println("Sum of a, b, c and d: " + (a + b + c + d));
     }
 
     public static void main(String[] args) {
         OverLodingSum obj = new OverLodingSum();
 
-        System.out.println("Sum of 10 and 20: " + obj.add(10, 20));
-        System.out.println("Sum of 10, 20 and 30: " + obj.add(10, 20, 30));
-        System.out.println("Sum of 10, 20, 30 and 40: " + obj.add(10, 20, 30, 40));
+        obj.add(10, 20);
+        obj.add(10, 20, 30);
+        obj.add(10, 20, 30, 40);
     }
 }

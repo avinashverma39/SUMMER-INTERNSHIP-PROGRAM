c6ce528d631b34c2908a3b4a6808e69efaed191c
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-17 15:36:35 +0530

    Sum of all array element

--- DAY_9/Array.java
+++ DAY_9/Array.java
@@ -5,6 +5,7 @@ package DAY_9;
 class Array {
 
 public static void main(String[] args) {
+    
     int arr[] = {10,20,30,40,50};
    for(int i = 0; i<5; i++)
    {
---
+++ DAY_9/sumofarray.java
@@ -0,0 +1,18 @@
+package DAY_9;
+
+public class sumofarray {
+
+    public static void main(String args[]) {
+
+        int a[] = { 10, 20, 30, 49 };
+
+        int sum = 0;
+        for (int i : a) {
+
+            sum += i;
+        }
+
+        System.out.println(sum);
+    }
+
+}

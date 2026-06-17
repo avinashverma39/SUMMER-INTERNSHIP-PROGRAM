bf4511e33ca2291c96dd5dab1a2156105002aaf2
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-17 15:03:56 +0530

    Odd number using array

--- DAY_9/ArrayNew.java
+++ DAY_9/ArrayNew.java
@@ -1,5 +1,18 @@
 package DAY_9;
+
 // Program using array  --------->
 public class ArrayNew {
-    
+
+    public static void main(String args[]) {
+
+        int arr[] = { 12, 11, 13, 15, 16, 18, 20 };
+        for (int i : arr) {
+            if (i % 2 != 0) {
+
+                System.out.println(i);
+
+            }
+        }
+    }
+
 }

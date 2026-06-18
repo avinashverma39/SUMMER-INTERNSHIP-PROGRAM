426cca125f364a21d0e694a5ea21717b6780fb06
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-18 13:33:30 +0530

    Sum of array using function

---
+++ DAY_10/ArrayFunction.java
@@ -0,0 +1,19 @@
+package DAY_10;
+
+public class ArrayFunction {
+    int arraySum(int a[]) {
+        int sum = 0;
+        for (int i : a) {
+            sum = sum + i;
+        }
+
+        return sum;
+    }
+
+    public static void main(String args[]) {
+        int arr[] = { 12, 13, 14, 15 };
+
+        ArrayFunction obj = new ArrayFunction();
+        System.out.println(obj.arraySum(arr));
+    }
+}

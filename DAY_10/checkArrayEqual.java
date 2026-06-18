c306693aa27af076b29dd5eeda95a82bf7256114
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-18 14:00:54 +0530

    Check tow array equal

---
+++ DAY_10/checkArrayEqual.java
@@ -0,0 +1,24 @@
+package DAY_10;
+
+public class checkArrayEqual {
+    boolean arr(int a[], int b[]) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+
+            if (a[i] != b[i]) {
+
+                count++;
+            }
+        }
+
+        return (count == 0) ? true : false;
+    }
+
+    public static void main(String args[]) {
+
+        int arr1[] = { 12, 13, 14, 15, 16, 17 };
+        int arr2[] = { 12, 13, 14, 15, 16, 17 };
+        checkArrayEqual obj = new checkArrayEqual();
+        System.out.println(obj.arr(arr1, arr2));
+    }
+}

e8ace5c1f35c10f205bad1b3057b691843cb0ad2
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-19 09:31:28 +0530

    3D Array

---
+++ DAY_11/Array3D.java
@@ -0,0 +1,22 @@
+package DAY_11;
+
+// Programe for 3 D array:
+public class Array3D {
+
+    public static void main(String[] args) {
+
+        int arr[][][] = { { { 12, 34, 57 }, { 56, 45, 78 }, { 90, 45, 56 }, { 78, 90, 67 } } };
+        for (int i = 0; i < 5; i++) {
+
+            for (int j = 0; j < 4; j++) {
+
+                for (int k = 0; k < 3; k++) {
+
+                    System.out.print(arr[i][j][k] + " | ");
+                }
+                System.out.println();
+
+            }
+        }
+    }
+}

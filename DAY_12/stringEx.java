53f35869f27e66bac173e1dea4e8a340018dbd69
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-22 15:08:11 +0530

    Add some string function

---
+++ DAY_12/stringEx.java
@@ -0,0 +1,18 @@
+package DAY_12;
+
+//String Example----------------->
+public class stringEx {
+
+    public static void main(String[] args) {
+        String s = "Avinash";
+
+        System.out.println(s);
+        System.out.println(s.length());
+        System.out.println(s.toUpperCase());
+        System.out.println(s.toLowerCase());
+        System.out.println(s.charAt(2));
+        System.out.println(s.concat(" Verma"));
+
+    }
+
+}

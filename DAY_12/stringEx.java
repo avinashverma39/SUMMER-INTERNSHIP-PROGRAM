53b9c5a520a040da73b50ef3395fa88aa4fa9dfd
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-22 15:20:05 +0530

    String in Java

--- DAY_12/stringEx.java
+++ DAY_12/stringEx.java
@@ -3,15 +3,23 @@ package DAY_12;
 //String Example----------------->
 public class stringEx {
 
-    public static void main(String[] args) {
-        String s = "Avinash";
+    // public static void main(String[] args) {
+    // String s = "Avinash";
 
+    // System.out.println(s);
+    // System.out.println(s.length());
+    // System.out.println(s.toUpperCase());
+    // System.out.println(s.toLowerCase());
+    // System.out.println(s.charAt(2));
+    // System.out.println(s.concat(" Verma"));
+
+    // }
+
+    public static void main(String[] args) {
+        String s = new String("Avinash Verma");
         System.out.println(s);
         System.out.println(s.length());
-        System.out.println(s.toUpperCase());
-        System.out.println(s.toLowerCase());
-        System.out.println(s.charAt(2));
-        System.out.println(s.concat(" Verma"));
+        
 
     }
 

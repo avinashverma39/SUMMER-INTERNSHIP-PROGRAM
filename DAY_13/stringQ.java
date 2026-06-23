90e8840ecaeca241337fb0e1cf9b4ace670b9478
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-23 12:49:36 +0530

    String Question

---
+++ DAY_13/stringQ.java
@@ -0,0 +1,13 @@
+package DAY_13;
+
+public class stringQ {
+
+    public static void main(String[] args) {
+        String fru = "Apple";
+
+        for (int i = 0; i < fru.length(); i++) {
+
+            System.out.println(fru.charAt(i));
+        }
+    }
+}

a5eced6d5375435ead64dd91001f32ced2890be7
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-23 13:53:48 +0530

    Revers String

---
+++ DAY_13/checkStrPalin.java
@@ -0,0 +1,11 @@
+package DAY_13;
+
+//to check pailndronm yes or no =----
+public class checkStrPalin {
+
+    public static void main(String[] args) {
+        StringBuilder s = new StringBuilder("abcdcba");
+        System.out.println(s.reverse());
+
+    }
+}

7040c59a78406221de7a07c2244fd9a42b11f8f7
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-23 15:30:56 +0530

    Some oops Concept

---
+++ OOPs/Students.java
@@ -0,0 +1,40 @@
+package OOPs;
+
+public class Students {
+
+    // FOUR PILLER OF OOPs
+
+    // 1. Inheritance
+    // 2. Polymorphism
+    // 3. Abstraction
+    // 4. Encapsulation
+
+    // constructor
+    // object creation
+    // method overloding
+    // method overridin
+    // Example
+
+    // class Students {
+
+    Students() // non parametrised constructor
+    {
+
+        System.out.println("This is Non-parametrised / Default Constructor");
+
+    }
+
+    Students(int a) // parameterized constructor
+    {
+        System.out.println("This is Parametrised Constructor " + a);
+
+    }
+
+    public static void main(String[] args) {
+
+     Students obj = new Students();
+      Students obj1 = new Students(20);
+
+    // }
+}
+}

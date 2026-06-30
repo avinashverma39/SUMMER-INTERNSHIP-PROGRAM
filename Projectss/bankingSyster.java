92bf194508176332ba09b496c51ccee60996665c
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-06-30 14:23:33 +0530

    Start Project Banking system

--- Projectss/bankingSyster.java
+++ Projectss/bankingSyster.java
@@ -7,6 +7,7 @@ class bankingSyster {
 
     int accno;
     String name;
+    String gmail;
     double balance;
     double withdrawl_amount;
     double deposit_amount;
@@ -17,10 +18,27 @@ class bankingSyster {
         this.balance = bal;
     }
 
+    void acDetial() {
+        System.out.println("Account Number : " + accno);
+        System.out.println("Account Holder Name: " + name);
+        System.out.println("Account gmail : " + gmail);
+
+    }
+
     void deposit(double amt) {
         balance = balance + amt;
 
         System.out.println("Your Account is Deposited with Rs. " + amt);
+        System.out.println("Current Balance Amount in your account Rs." + balance);
+
+    }
+
+    void withdraw(double amt) {
+
+        balance = balance - amt;
+
+        System.out.println("Your Account is Withdrawl with Rs. " + amt);
+        System.out.println("Current Balance Amount is Rs. " + balance);
 
     }
 

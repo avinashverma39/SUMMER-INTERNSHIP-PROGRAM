f5b3102534291e6a9556cdb3cda08bda2ef9c909
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-03 14:36:06 +0530

    Sports package use in JavaPackage

---
+++ Sports/cricket.java
@@ -0,0 +1,9 @@
+package Sports;
+
+public class cricket {
+
+    public void playerName(String name) {
+
+        System.out.println("Cricketer name is: " + name);
+    }
+}
---
+++ Sports/football.java
@@ -0,0 +1,10 @@
+package Sports;
+
+public class football {
+
+    public void playerName(String name) {
+
+        System.out.println("player name is: " + name);
+    }
+
+}

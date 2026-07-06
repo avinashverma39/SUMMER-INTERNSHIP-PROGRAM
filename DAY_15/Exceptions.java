7be46987d5170939e7ab752f3ae80213ea5577b6
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-06 13:13:42 +0530

    Exception Handling In array bound

--- DAY_15/Exceptions.java
+++ DAY_15/Exceptions.java
@@ -4,14 +4,9 @@ public class Exceptions {
     public static void main(String args[]) {
 
         System.out.println("Program Start");
-        int c = 10;
-        int b = 0;
-        int r;
+     
 
         try {
-
-            r = c / b;
-            System.out.println(r);
 
             int a[] = { 10, 11, 12 };
             System.out.println(a[4]);
@@ -21,15 +16,6 @@ public class Exceptions {
             System.out.println(e);
             System.out.println("Exception is handled");
         }
-
-        catch (ArithmeticException e) {
-            System.out.println(e);
-            System.out.println("Divide by zero is not allowed");
-        }
-
-        // finally {
-        // System.out.println("Finally block is always executed");
-        // }
 
         System.out.println("Program End");
     }
---
+++ DAY_15/MultipalEx.java
@@ -0,0 +1,42 @@
+package DAY_15;
+
+public class MultipalEx {
+
+
+        public static void main(String args[]) {
+
+        System.out.println("Program Start");
+        int c = 10;
+        int b = 0;
+        int r;
+
+        try {
+
+            r = c / b;
+            System.out.println(r);
+
+            int a[] = { 10, 11, 12 };
+            System.out.println(a[4]);
+        }
+
+        catch (ArrayIndexOutOfBoundsException e) {
+            System.out.println(e);
+            System.out.println("Exception is handled");
+        }
+
+        catch (ArithmeticException e) {
+            System.out.println(e);
+            System.out.println("Divide by zero is not allowed");
+        }
+
+        // finally {
+        // System.out.println("Finally block is always executed");
+        // }
+
+        System.out.println("Program End");
+    }
+
+}
+
+    
+

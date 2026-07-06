e203ed3dc405770098179c72cec086bae79d1e26
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-06 12:57:22 +0530

    Array index out of bound exception

---
+++ DAY_15/Exceptions.java
@@ -0,0 +1,24 @@
+package DAY_15;
+
+public class Exceptions {
+    public static void main(String args[]) {
+
+        System.out.println("Program Start");
+
+        try {
+
+            int a[] = { 10, 11, 12 };
+            System.out.println(a[4]);
+        }
+
+        catch (
+
+        ArrayIndexOutOfBoundsException e) {
+            // System.out.println(e);
+            System.out.println("Exception is handled");
+        }
+
+        System.out.println("Program End");
+    }
+
+}

b47c6c2bbc223dfdd6fa185c9cdeebbf26d0ab53
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-06 14:26:51 +0530

    Wapper class/object

--- DAY_15/wapperClass.java
+++ DAY_15/wapperClass.java
@@ -2,6 +2,30 @@ package DAY_15;
 
 // is a pre define class in java that wraps primitive data types into an objects. It is a part of java.lang package. so that it can be used where object can required.
 // Many java feature works only object such as collection freamwork, generic, springboot,hyperNet,javaStream etc. so wrapper class is used to convert primitive data type into object and vice versa.
+
+/* 
+
+primitive data type                    |
+                                       |          object/wrapper class
+     int                               |   integer                             
+      short                            |    Short                             
+        byte                           |   Byte
+           long                        |   Long
+                float                  |   Float
+                     double            |   Double
+                           char        |   Character
+                             boolean   |   Boolean
+   
+
+
+                              Wrapper class is used to convert primitive data type into object and vice versa. It is a part of java.lang package. so that it can be used where object can required.
+
+
+      Boxing and Unboxing------>
+                    converting a primitive data type into object is called boxing and converting an object into primitive data type is called unboxing.
+
+*/
+
 public class wapperClass {
 
 }

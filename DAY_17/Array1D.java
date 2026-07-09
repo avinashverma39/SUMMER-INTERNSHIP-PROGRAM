db5c238f748cdb921dd0711869dc8cb4c5c6a1b2
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-09 09:58:00 +0530

    DSA foundation start in java

---
+++ DAY_17/Array1D.java
@@ -0,0 +1,25 @@
+package DAY_17;
+
+//Array using function--
+public class Array1D {
+
+    public static void Array1D_w(int n[]) {
+
+        int arr[] = { 10, 30, 40, 50, 60 };
+
+        for (int i = 0; i < arr.length; i++) {
+
+            System.out.println(arr[i]);
+        }
+
+        System.out.println("Array length is: " + arr.length);
+
+    }
+
+    public static void main(String args[]) {
+
+        Array1D_w(null);
+
+    }
+
+}

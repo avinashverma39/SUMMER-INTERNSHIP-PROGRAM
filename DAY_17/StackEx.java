f7908cbca131e9415019fe4d899d42a8cd7eeec7
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-09 10:44:17 +0530

    Stack arrangment in java

--- DAY_17/StackEx.java
+++ DAY_17/StackEx.java
@@ -1,3 +1,5 @@
+//Stack implement using array--------->
+
 package DAY_17;
 
 public class StackEx {
@@ -21,9 +23,37 @@ public class StackEx {
         }
         stack[++top] = element;
         System.out.println("Element insernted successfully " + element);
+
     }
 
-    public static void main(String args[]){
+    void pop() {
+        if (top == size - 1) {
+            System.out.println("Stack under flow!!");
+            return;
+        }
+        System.out.println("/nElement remove successfully " + stack[top]);
+        top--;
+    }
+
+    void display() {
+        if (top == -1) {
+            System.out.println("Stack is empty!!");
+            return;
+        }
+        for (int i = top; i >= 0; i--) {
+            System.out.print(stack[i] + " ");
+        }
+    }
+
+    void peek() {
+        if (top == -1) {
+            System.out.println("Stack is empty!!");
+            return;
+        }
+        System.out.println("Top element is: " + stack[top]);
+    }
+
+    public static void main(String args[]) {
 
         StackEx obj = new StackEx(6);
         obj.push(122);
@@ -32,5 +62,15 @@ public class StackEx {
         obj.push(120);
         obj.push(120);
         obj.push(1320);
-       }
+        obj.peek();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.pop();
+        obj.peek();
+        obj.display();
+
+    }
 }

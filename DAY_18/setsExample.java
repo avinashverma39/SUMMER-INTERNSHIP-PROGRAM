d1d9a61bb72f4df43fe36e66aa3082ab1a119b6d
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-11 14:50:07 +0530

    Sets example in java

--- DAY_18/setsExample.java
+++ DAY_18/setsExample.java
@@ -16,37 +16,53 @@ public class setsExample {
         System.out.println("Set 3: " + s.set3);
         System.out.println("Set 4: " + s.set4);
         System.out.println("Set 5: " + s.set5);
+        System.out.println("Set 6: " + s.set6);
+        System.out.println("Set 7: " + s.set7);
     }
 
     Set<Integer> set1 = new HashSet<>();
     Set<Integer> set2 = new HashSet<>();
     LinkedHashSet<Integer> set4 = new LinkedHashSet<>();
+    LinkedHashSet<String> set6 = new LinkedHashSet<>();
     Set<String> set3 = new HashSet<>();
     TreeSet<Integer> set5 = new TreeSet<>();
+    TreeSet<String> set7 = new TreeSet<>();
 
     setsExample() {
         set1.add(1);
         set1.add(2);
         set2.add(3);
         set2.add(4);
-    
+
         set4.add(13);
         set4.add(43);
         set4.add(33);
         set4.add(44);
-
 
         set5.add(45);
         set5.add(24);
         set5.add(33);
         set5.add(44);
 
+        set6.add("Java");
+        set6.add("Python");
+        set6.add("C++");
+        set6.add("JavaScript");
+        set6.add("java");
 
-        set3.add("Java");
-        set3.add("Python");
-        set3.add("C++");
-        set3.add("JavaScript");
-        set3.add("java");
+        for (int i = 0; i < 10; i++) {
+            set3.add("Number " + i);
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            set3.add("Number " + i);
+        }
+
+        set7.add("Java");
+        set7.add("Python");
+        set7.add("C++");
+        set7.add("JavaScript");
+        set7.add("java");
     }
 
 }

13f191e5491b6d236862a27d108a19ff91313de5
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-11 14:38:47 +0530

    Sets example in java

--- DAY_18/setsExample.java
+++ DAY_18/setsExample.java
@@ -1,8 +1,9 @@
 package DAY_18;
 
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
-// import java.util.*;
+import java.util.*;
 
 //program to implement sets in java
 public class setsExample {
@@ -13,18 +14,33 @@ public class setsExample {
         System.out.println("Set 1: " + s.set1);
         System.out.println("Set 2: " + s.set2);
         System.out.println("Set 3: " + s.set3);
+        System.out.println("Set 4: " + s.set4);
+        System.out.println("Set 5: " + s.set5);
     }
 
     Set<Integer> set1 = new HashSet<>();
     Set<Integer> set2 = new HashSet<>();
-
+    LinkedHashSet<Integer> set4 = new LinkedHashSet<>();
     Set<String> set3 = new HashSet<>();
+    TreeSet<Integer> set5 = new TreeSet<>();
 
     setsExample() {
         set1.add(1);
         set1.add(2);
         set2.add(3);
         set2.add(4);
+    
+        set4.add(13);
+        set4.add(43);
+        set4.add(33);
+        set4.add(44);
+
+
+        set5.add(45);
+        set5.add(24);
+        set5.add(33);
+        set5.add(44);
+
 
         set3.add("Java");
         set3.add("Python");

4edc48ac1c98e171348901f808872cffded3daab
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-11 14:30:45 +0530

    Sets example in java

--- DAY_18/setsExample.java
+++ DAY_18/setsExample.java
@@ -2,25 +2,35 @@ package DAY_18;
 
 import java.util.HashSet;
 import java.util.Set;
-import java.util.*;
+// import java.util.*;
 
 //program to implement sets in java
 public class setsExample {
 
     public static void main(String[] args) {
+
         setsExample s = new setsExample();
         System.out.println("Set 1: " + s.set1);
         System.out.println("Set 2: " + s.set2);
+        System.out.println("Set 3: " + s.set3);
     }
 
     Set<Integer> set1 = new HashSet<>();
     Set<Integer> set2 = new HashSet<>();
+
+    Set<String> set3 = new HashSet<>();
 
     setsExample() {
         set1.add(1);
         set1.add(2);
         set2.add(3);
         set2.add(4);
+
+        set3.add("Java");
+        set3.add("Python");
+        set3.add("C++");
+        set3.add("JavaScript");
+        set3.add("java");
     }
 
 }

3077735a116e5ec0c407a9af836c909cb819121b
Avinash Verma <205390059+avinashverma39@users.noreply.github.com>
2026-07-14 10:11:31 +0530

    Flie Handling in java

--- DAY_19/SerializeStudent.java
+++ DAY_19/SerializeStudent.java
@@ -2,6 +2,7 @@ package DAY_19;
 
 import java.io.FileOutputStream;
 import java.io.ObjectOutputStream;
+import java.util.ArrayList;
 import java.io.IOException;
 
 public class SerializeStudent {
@@ -9,15 +10,23 @@ public class SerializeStudent {
     public static void main(String[] args) {
 
         try {
+
+            ArrayList<fileHendling> students = new ArrayList<>();
             // Create a new student object
-            fileHendling student = new fileHendling(10112, "John Doe", "Computer Science");
+            students.add(new fileHendling(10112, "John Doe", "Computer Science"));
+            students.add(new fileHendling(10113, "Jane Smith", "Mathematics"));
+            students.add(new fileHendling(10114, "Alice Johnson", "Physics"));
+            // Serialize the ArrayList of Student objects to a file
+            
             ObjectOutputStream out = new ObjectOutputStream(new FileOutputStream("student.dat"));
-            out.writeObject(student);
+            out.writeObject(students);
             out.close();
-            System.out.println("Student object serialized successfully.");
+            System.out.println("Student objects serialized successfully.");
 
             // Display student details
-            student.displayDetails();
+            for (fileHendling s : students) {
+                s.displayDetails();
+            }
         }
 
         catch (IOException e) {
